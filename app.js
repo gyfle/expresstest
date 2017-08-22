366d9a0ca630e903b7acbf231f5838e29eb28e89
Lars Winther Madsen <lwm@danskecommodities.com>
2017-08-22 12:41:15 +0200

    Add SQL module template

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Node Express Up and Running!')
 })
 
 app.listen(3000, function () {
